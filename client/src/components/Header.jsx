5fa08f8e42a3c125eb32e43d05e31b60172c1599
blood <chayanmann09@gmail.com>
2025-04-04 14:03:50 +0530

    minor change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,7 +18,9 @@ function Header() {
             d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
           />
         </svg>
-        <h1 className="text-2xl font-bold text-gray-800">Health Ledger</h1>
+        <Link to="/">
+          <h1 className="text-2xl font-bold text-gray-800">Health Ledger</h1>
+        </Link>
       </div>
       <nav>
         <ul className="flex space-x-6">
